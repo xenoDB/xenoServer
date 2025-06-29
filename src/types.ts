9156ee8d804639cbbb23b1255faddc8bf13afb6d
painfueg0 <kystax69@gmail.com>
2025-06-29 22:03:46 +0530

    upd types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,20 @@
 /** @format */
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
 
-export type Payload<T = unknown> =
-  | { path: string; requestId: string; method: "ALL" }
-  | { path: string; requestId: string; method: "SET"; key: string; value: T }
-  | { path: string; requestId: string; method: "GET" | "DELETE" | "HAS"; key: string }
-  | { path: string; requestId: string; method: "GET_MANY" | "DELETE_MANY"; keys: string[] }
-  | { path: string; requestId: string; method: "SET_MANY"; data: { key: string; value: T }[] };
+export interface BasePayload {
+  path: string;
+  requestId: string;
+}
+
+export type Payload<T = unknown> = Prettify<
+  BasePayload &
+    (
+      | { method: "ALL" }
+      | { method: "SET"; key: string; value: T }
+      | { method: "GET" | "DELETE" | "HAS"; key: string }
+      | { method: "GET_MANY" | "DELETE_MANY"; keys: string[] }
+      | { method: "SET_MANY"; data: { key: string; value: T }[] }
+    )
+>;
