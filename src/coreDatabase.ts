923043e7046806ec5c23ae7e946d322a5c5c1871
painfueg0 <kystax69@gmail.com>
2025-06-29 15:12:08 +0530

    rem linter  and format

--- src/coreDatabase.ts
+++ src/coreDatabase.ts
@@ -96,7 +96,7 @@ export class CoreDatabase<T> {
   #debouncedWrite() {
     this.#debounceCount++;
 
-    if (this.#debounceCount >= this.#maxDebounceCount) return (this.#debounceCount = 0), this.#write();
+    if (this.#debounceCount >= this.#maxDebounceCount) return ((this.#debounceCount = 0), this.#write());
 
     this.#timer?.refresh();
 
@@ -164,9 +164,12 @@ export class CoreDatabase<T> {
   }
 
   all(): { [K: string]: T } {
-    return this.#cache.values().reduce((prev, curr) => {
-      for (const [key, value] of Object.entries(curr)) prev[key] = value;
-      return prev;
-    }, {} as { [K: string]: T });
+    return this.#cache.values().reduce(
+      (prev, curr) => {
+        for (const [key, value] of Object.entries(curr)) prev[key] = value;
+        return prev;
+      },
+      {} as { [K: string]: T }
+    );
   }
 }
