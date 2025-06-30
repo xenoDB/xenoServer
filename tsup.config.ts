cc94550bb5353171f701e5c6c97b2da103227b26
painfueg0 <kystax69@gmail.com>
2025-06-30 20:36:40 +0530

    tsup

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+/** @format */
+
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  dts: true,
+  clean: true,
+  outDir: "lib",
+  minify: false,
+  treeshake: true,
+  target: "esnext",
+  platform: "node",
+  format: ["cjs", "esm"],
+  experimentalDts: false,
+  entry: ["src/index.ts"],
+  removeNodeProtocol: false,
+  skipNodeModulesBundle: true
+});
